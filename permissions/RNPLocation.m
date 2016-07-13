6b1c0f1722bda0a458edd3503630b5bed3ee4d65
Radu Popovici <radu.popovici@munichgeneral.com>
2016-07-13 17:30:07 +0300

    call request authorization on main thread and ignore the first call for didChangeAuthorization

--- permissions/RNPLocation.m
+++ permissions/RNPLocation.m
@@ -37,32 +37,39 @@
 {
     NSString *status = [RNPLocation getStatus];
     if (status == RNPStatusUndetermined) {
-        self.completionHandler = completionHandler;
-        
-        self.locationManager = [[CLLocationManager alloc] init];
-        self.locationManager.delegate = self;
-        if ([type isEqualToString:@"always"]) {
-            [self.locationManager requestAlwaysAuthorization];
-        } else {
-            [self.locationManager requestWhenInUseAuthorization];
-        }
+        dispatch_async(dispatch_get_main_queue(), ^(){
+            self.completionHandler = completionHandler;
+            
+            self.locationManager = [[CLLocationManager alloc] init];
+            self.locationManager.delegate = self;
+                
+            if ([type isEqualToString:@"always"]) {
+                [self.locationManager requestAlwaysAuthorization];
+            } else {
+                [self.locationManager requestWhenInUseAuthorization];
+            }
+        });
     } else {
         completionHandler(status);
     }
 }
 
--(void)locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status {
-    if (self.locationManager) {
-        self.locationManager.delegate = nil;
-        self.locationManager = nil;
-    }
-    
-    if (self.completionHandler) {
-        //for some reason, checking permission right away returns denied. need to wait a tiny bit
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.1 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
-            self.completionHandler([RNPLocation getStatus]);
-            self.completionHandler = nil;
-        });
+-(void)locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status
+{
+    if (status != kCLAuthorizationStatusNotDetermined) {
+        if (self.locationManager) {
+            self.locationManager.delegate = nil;
+            self.locationManager = nil;
+        }
+
+        if (self.completionHandler) {
+            //for some reason, checking permission right away returns denied. need to wait a tiny bit
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.1 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+                self.completionHandler([RNPLocation getStatus]);
+                self.completionHandler = nil;
+            });
+        }
     }
 }
+
 @end
