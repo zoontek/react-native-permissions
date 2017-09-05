88122d3164c06f0d9b3f752279c0e2535d196d39
huhuanming <workboring@gmail.com>
2017-09-05 11:24:55 +0800

    Update ReactNativePermissions.m

--- ReactNativePermissions.m
+++ ReactNativePermissions.m
@@ -10,26 +10,10 @@
 
 #import "ReactNativePermissions.h"
 
-#if __has_include(<React/RCTBridge.h>)
-  #import <React/RCTBridge.h>
-#elif __has_include("RCTBridge.h")
-  #import "RCTBridge.h"
-#else
-  #import "React/RCTBridge.h"
-#endif
-
-
-#if __has_include("RCTConvert.h")
-  #import "RCTConvert.h"
-#else
-  #import <React/RCTConvert.h>
-#endif
-
-#if __has_include("RCTEventDispatcher.h")
-  #import "RCTEventDispatcher.h"
-#else
-  #import <React/RCTEventDispatcher.h>
-#endif
+#import "ReactNativePermissions.h"
+#import <React/RCTBridge.h>
+#import <React/RCTConvert.h>
+#import <React/RCTEventDispatcher.h>
 
 #import "RNPLocation.h"
 #import "RNPBluetooth.h"
