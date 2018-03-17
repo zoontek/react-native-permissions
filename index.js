3e3bfe103f8688066c714077486313715b6ad56e
Joseph Buchma <josephbuchma@gmail.com>
2018-03-18 00:27:46 +0200

    Fix flow error (#230)

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 // @flow
 
-import Permissions from './lib/permissions'
+import { Platform } from 'react-native'
+
+import PermissionsAndroid from './lib/android'
+import PermissionsIOS from './lib/ios'
+
+const Permissions = Platform.OS === 'ios' ? PermissionsIOS : PermissionsAndroid
+
 export default Permissions
