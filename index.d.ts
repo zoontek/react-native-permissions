aa0ee9eb0bd26fd64ec34f8f041e25f580a2a10f
Henrik Raitasola <henrik.raitasola@gmail.com>
2018-04-04 13:50:30 +0300

    Add more types

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,9 @@
+type PermissionType = 'location' | 'camera' | 'microphone' | 'photo' | 'contacts' | 'event';
+
 export function check(
-  permission: 'location' | 'camera' | 'microphone' | 'photo' | 'contacts' | 'event',
+  permission: PermissionType,
 ): Promise<'authorized' | 'denied' | 'restricted' | 'undetermined'>;
+export function request(
+  permission: PermissionType,
+): Promise<'authorized' | 'denied' | 'restricted' | 'undetermined'>;
+export function openSettings(): void;
