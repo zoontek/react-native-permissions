db07174c73bb24a43144b23bd69e8d42325085b5
Henrik Raitasola <henrik.raitasola@gmail.com>
2018-04-04 13:56:01 +0300

    Export response because you might need it

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 type PermissionType = 'location' | 'camera' | 'microphone' | 'photo' | 'contacts' | 'event';
-type Response = 'authorized' | 'denied' | 'restricted' | 'undetermined';
+export type Response = 'authorized' | 'denied' | 'restricted' | 'undetermined';
 
 export function check(permission: PermissionType): Promise<Response>;
 export function request(permission: PermissionType): Promise<Response>;
