2ec6dec33bc0419f7ea383cb36dbb3f221a63b34
Mathieu Acthernoene <zoontek@gmail.com>
2019-07-18 12:48:19 +0200

    Fix MPMediaLibrary not available on simulators / iOS < 9.3

--- ios/Permissions/RNPMediaLibrary.m
+++ ios/Permissions/RNPMediaLibrary.m
@@ -13,28 +13,45 @@
 
 + (NSString *)getStatus
 {
-    switch ([MPMediaLibrary authorizationStatus]) {
-        case MPMediaLibraryAuthorizationStatusAuthorized:
-            return RNPStatusAuthorized;
-        case MPMediaLibraryAuthorizationStatusDenied:
-            return RNPStatusDenied;
-        case MPMediaLibraryAuthorizationStatusRestricted:
-            return RNPStatusRestricted;
-        default:
-            return RNPStatusUndetermined;
+#if TARGET_OS_SIMULATOR
+    return RNPStatusRestricted;
+#else
+    if (@available(iOS 9.3, *)) {
+        switch ([MPMediaLibrary authorizationStatus]) {
+            case MPMediaLibraryAuthorizationStatusAuthorized:
+                return RNPStatusAuthorized;
+            case MPMediaLibraryAuthorizationStatusDenied:
+                return RNPStatusDenied;
+            case MPMediaLibraryAuthorizationStatusRestricted:
+                return RNPStatusRestricted;
+            default:
+                return RNPStatusUndetermined;
+        }
+    } else {
+        return RNPStatusAuthorized;
     }
+#endif
 }
 
 + (void)request:(void (^)(NSString *))completionHandler
 {
-    void (^handler)(void) =  ^(void) {
-        dispatch_async(dispatch_get_main_queue(), ^{
-            completionHandler([self.class getStatus]);
-        });
-    };
+#if TARGET_OS_SIMULATOR
+    completionHandler(RNPStatusRestricted);
+#else
+    if (@available(iOS 9.3, *)) {
+        void (^handler)(void) =  ^(void) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                completionHandler([self.class getStatus]);
+            });
+        };
 
-    [MPMediaLibrary requestAuthorization:^(MPMediaLibraryAuthorizationStatus status){
-        handler();
-    }];
+        [MPMediaLibrary requestAuthorization:^(MPMediaLibraryAuthorizationStatus status){
+            handler();
+        }];
+    } else {
+        completionHandler(RNPStatusAuthorized);
+    }
+#endif
 }
+
 @end
