54ac46fce0407e1f40ea9e4229d7611a45670bbb
Mathieu Acthernoene <zoontek@gmail.com>
2019-09-19 12:10:45 +0200

    Add a warning about bad install
    Thanks to https://github.com/react-native-community/react-native-netinfo/blob/v4.2.1/src/internal/nativeInterface.ts#L15

--- src/index.ts
+++ src/index.ts
@@ -98,22 +98,36 @@ export interface NotificationsResponse {
 }
 
 const RNPermissions: {
+  // Android + iOS
+  checkNotifications: () => Promise<NotificationsResponse>;
+  requestNotifications: (
+    options: NotificationOption[],
+  ) => Promise<NotificationsResponse>;
   openSettings: () => Promise<true>;
+
+  // Android only
+  isAvailable: (permission: Permission) => Promise<boolean>;
+  getNonRequestables: () => Promise<Permission[]>;
+  setNonRequestable: (permission: Permission) => Promise<true>;
+
+  // iOS only
   check: (permission: Permission) => Promise<PermissionStatus>;
   request: (
     permission: Permission,
     rationale?: Rationale,
   ) => Promise<PermissionStatus>;
-  checkNotifications: () => Promise<NotificationsResponse>;
-  requestNotifications: (
-    options: NotificationOption[],
-  ) => Promise<NotificationsResponse>;
-
-  // android only module methods
-  isAvailable: (permission: Permission) => Promise<boolean>;
-  setNonRequestable: (permission: Permission) => Promise<true>;
-  getNonRequestables: () => Promise<Permission[]>;
 } = NativeModules.RNPermissions;
+
+// Produce an error if we don't have the native module
+if (RNPermissions == null) {
+  throw new Error(`react-native-permissions: NativeModule.RNPermissions is null. To fix this issue try these steps:
+• Run \`react-native link react-native-permissions\` in the project root.
+• Rebuild and re-run the app.
+• If you are using CocoaPods on iOS, run \`pod install\` in the \`ios\` directory and then rebuild and re-run the app. You may also need to re-open Xcode to get the new pods.
+• Check that the library was linked correctly when you used the link command by running through the manual installation instructions in the README.
+* If you are getting this error while unit testing you need to mock the native module. Follow the guide in the README.
+If none of these fix the issue, please open an issue on the Github repository: https://github.com/react-native-community/react-native-permissions`);
+}
 
 const platformPermissions = Object.values(
   Platform.OS === 'ios' ? IOS : ANDROID,
