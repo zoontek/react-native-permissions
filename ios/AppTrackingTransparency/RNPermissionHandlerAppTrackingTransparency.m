0a972deae6504ebc94cf08884d265fffe91cde44
Mathieu Acthernoene <zoontek@gmail.com>
2020-08-27 10:33:21 +0200

    Fix indent

--- ios/AppTrackingTransparency/RNPermissionHandlerAppTrackingTransparency.m
+++ ios/AppTrackingTransparency/RNPermissionHandlerAppTrackingTransparency.m
@@ -15,35 +15,36 @@
 
 - (void)checkWithResolver:(void (^ _Nonnull)(RNPermissionStatus))resolve
                  rejecter:(void (__unused ^ _Nonnull)(NSError * _Nonnull))reject {
-    if (@available(iOS 14.0, *)) {
-      switch ([ATTrackingManager trackingAuthorizationStatus]) {
-        case ATTrackingManagerAuthorizationStatusNotDetermined:
-          return resolve(RNPermissionStatusNotDetermined);
-        case ATTrackingManagerAuthorizationStatusRestricted:
-          return resolve(RNPermissionStatusRestricted);
-        case ATTrackingManagerAuthorizationStatusDenied:
-          return resolve(RNPermissionStatusDenied);
-        case ATTrackingManagerAuthorizationStatusAuthorized:
-          return resolve(RNPermissionStatusAuthorized);
-      }
-    } else {
-        NSString *idfaString = [[[ASIdentifierManager sharedManager] advertisingIdentifier] UUIDString];
-        if ([idfaString isEqualToString:@"00000000-0000-0000-0000-000000000000"]) {
-            return resolve(RNPermissionStatusDenied);
-        }
-        
-        resolve(RNPermissionStatusAuthorized);
+  if (@available(iOS 14.0, *)) {
+    switch ([ATTrackingManager trackingAuthorizationStatus]) {
+      case ATTrackingManagerAuthorizationStatusNotDetermined:
+        return resolve(RNPermissionStatusNotDetermined);
+      case ATTrackingManagerAuthorizationStatusRestricted:
+        return resolve(RNPermissionStatusRestricted);
+      case ATTrackingManagerAuthorizationStatusDenied:
+        return resolve(RNPermissionStatusDenied);
+      case ATTrackingManagerAuthorizationStatusAuthorized:
+        return resolve(RNPermissionStatusAuthorized);
     }
+  } else {
+    NSString *idfaString = [[[ASIdentifierManager sharedManager] advertisingIdentifier] UUIDString];
+
+    if ([idfaString isEqualToString:@"00000000-0000-0000-0000-000000000000"]) {
+      resolve(RNPermissionStatusDenied);
+    } else {
+      resolve(RNPermissionStatusAuthorized);
+    }
+  }
 }
 
 - (void)requestWithResolver:(void (^ _Nonnull)(RNPermissionStatus))resolve
                    rejecter:(void (^ _Nonnull)(NSError * _Nonnull))reject {
   if (@available(iOS 14.0, *)) {
     [ATTrackingManager requestTrackingAuthorizationWithCompletionHandler:^(__unused ATTrackingManagerAuthorizationStatus status) {
-        [self checkWithResolver:resolve rejecter:reject];
+      [self checkWithResolver:resolve rejecter:reject];
     }];
   } else {
-      [self checkWithResolver:resolve rejecter:reject];
+    [self checkWithResolver:resolve rejecter:reject];
   }
 }
 
