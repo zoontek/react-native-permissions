ecb49869ba5ee8412bc75346875b569ab1c5afe7
Mathieu Acthernoene <zoontek@gmail.com>
2020-10-10 15:58:33 +0200

    Drop iOS 9 support for MediaLibrary and StoreKit

--- ios/StoreKit/RNPermissionHandlerStoreKit.m
+++ ios/StoreKit/RNPermissionHandlerStoreKit.m
@@ -17,19 +17,15 @@
 #if TARGET_OS_SIMULATOR
   return resolve(RNPermissionStatusNotAvailable);
 #else
-  if (@available(iOS 9.3, *)) {
-    switch ([SKCloudServiceController authorizationStatus]) {
-      case SKCloudServiceAuthorizationStatusNotDetermined:
-        return resolve(RNPermissionStatusNotDetermined);
-      case SKCloudServiceAuthorizationStatusRestricted:
-        return resolve(RNPermissionStatusRestricted);
-      case SKCloudServiceAuthorizationStatusDenied:
-        return resolve(RNPermissionStatusDenied);
-      case SKCloudServiceAuthorizationStatusAuthorized:
-        return resolve(RNPermissionStatusAuthorized);
-    }
-  } else {
-    resolve(RNPermissionStatusNotAvailable);
+  switch ([SKCloudServiceController authorizationStatus]) {
+    case SKCloudServiceAuthorizationStatusNotDetermined:
+      return resolve(RNPermissionStatusNotDetermined);
+    case SKCloudServiceAuthorizationStatusRestricted:
+      return resolve(RNPermissionStatusRestricted);
+    case SKCloudServiceAuthorizationStatusDenied:
+      return resolve(RNPermissionStatusDenied);
+    case SKCloudServiceAuthorizationStatusAuthorized:
+      return resolve(RNPermissionStatusAuthorized);
   }
 #endif
 }
@@ -37,13 +33,9 @@
 - (void)requestWithResolver:(void (^ _Nonnull)(RNPermissionStatus))resolve
                    rejecter:(void (^ _Nonnull)(NSError * _Nonnull))reject
                     options:(NSDictionary *_Nullable)options {
-  if (@available(iOS 9.3, *)) {
-    [SKCloudServiceController requestAuthorization:^(__unused SKCloudServiceAuthorizationStatus status) {
-      [self checkWithResolver:resolve rejecter:reject];
-    }];
-  } else {
-    resolve(RNPermissionStatusNotAvailable);
-  }
+  [SKCloudServiceController requestAuthorization:^(__unused SKCloudServiceAuthorizationStatus status) {
+    [self checkWithResolver:resolve rejecter:reject];
+  }];
 }
 
 @end
