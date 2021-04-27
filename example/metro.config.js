970907d5d61ec9750b3164991a5489ebcc44b568
Mathieu Acthernoene <zoontek@gmail.com>
2021-04-27 17:45:27 +0200

    Update example project

--- example/metro.config.js
+++ example/metro.config.js
@@ -16,7 +16,7 @@ module.exports = {
     getTransformOptions: async () => ({
       transform: {
         experimentalImportSupport: false,
-        inlineRequires: false,
+        inlineRequires: true,
       },
     }),
   },
