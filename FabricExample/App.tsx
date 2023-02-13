80e07a33cb225356b70de7a7ebf8b2718c415a4e
Wojciech Lewicki <wojciech.lewicki@swmansion.com>
2023-02-13 18:41:57 +0100

    feat: add code from example

--- FabricExample/App.tsx
+++ FabricExample/App.tsx
@@ -1,118 +1,217 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import * as React from 'react';
+import {Platform, ScrollView, StatusBar, View} from 'react-native';
+import {Appbar, Button, Divider, Snackbar, Text} from 'react-native-paper';
+import RNPermissions, {
+  NotificationOption,
+  Permission,
+  PERMISSIONS,
+} from 'react-native-permissions';
+import theme from './theme';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const {SIRI, ...PERMISSIONS_IOS} = PERMISSIONS.IOS; // remove siri (certificate required)
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+const PLATFORM_PERMISSIONS = Platform.select<
+  | typeof PERMISSIONS.ANDROID
+  | typeof PERMISSIONS_IOS
+  | typeof PERMISSIONS.WINDOWS
+  | {}
+>({
+  android: PERMISSIONS.ANDROID,
+  ios: PERMISSIONS_IOS,
+  windows: PERMISSIONS.WINDOWS,
+  default: {},
 });
 
-export default App;
+const PERMISSIONS_VALUES: Permission[] = Object.values(PLATFORM_PERMISSIONS);
+
+export const App = () => {
+  const [snackbarContent, setSnackbarContent] = React.useState<string>();
+
+  const showSnackbar = (title: string, response: unknown) =>
+    setSnackbarContent(title + '\n\n' + JSON.stringify(response, null, 2));
+
+  const hideSnackbar = () => setSnackbarContent(undefined);
+
+  return (
+    <View style={{flex: 1, backgroundColor: theme.colors.background}}>
+      <StatusBar
+        backgroundColor={theme.colors.primary}
+        barStyle="light-content"
+      />
+
+      <Appbar.Header>
+        <Appbar.Content
+          title="react-native-permissions"
+          subtitle="Example application"
+        />
+
+        {Platform.OS === 'ios' && (
+          <Appbar.Action
+            icon="image-multiple"
+            onPress={() => {
+              RNPermissions.openLimitedPhotoLibraryPicker().catch(error => {
+                console.error(error);
+              });
+            }}
+          />
+        )}
+
+        {Platform.OS === 'ios' && (
+          <Appbar.Action
+            icon="crosshairs-question"
+            onPress={() => {
+              RNPermissions.requestLocationAccuracy({
+                purposeKey: 'full-accuracy',
+              }).then(accuracy => {
+                console.warn({accuracy});
+              });
+            }}
+          />
+        )}
+
+        <Appbar.Action
+          icon="cellphone-cog"
+          onPress={() => {
+            RNPermissions.openSettings();
+          }}
+        />
+      </Appbar.Header>
+
+      <ScrollView>
+        {PERMISSIONS_VALUES.map((item, index) => {
+          const value = PERMISSIONS_VALUES[index];
+          const parts = item.split('.');
+          const name = parts[parts.length - 1];
+
+          return (
+            <React.Fragment key={item}>
+              <View style={{padding: 20}}>
+                <Text
+                  numberOfLines={1}
+                  style={{
+                    color: 'rgba(0, 0, 0, 0.87)',
+                    fontWeight: '400',
+                    fontSize: 16,
+                    textAlign: 'left',
+                  }}>
+                  {name}
+                </Text>
+
+                <View style={{flexDirection: 'row', marginTop: 12}}>
+                  <Button
+                    compact={true}
+                    icon="eye-outline"
+                    mode="outlined"
+                    onPress={() => {
+                      RNPermissions.check(value)
+                        .then(status => {
+                          showSnackbar(`check(${name})`, status);
+                        })
+                        .catch(error => {
+                          console.error(error);
+                        });
+                    }}>
+                    Check
+                  </Button>
+
+                  <View style={{width: 8}} />
+
+                  <Button
+                    compact={true}
+                    icon="help-circle-outline"
+                    mode="outlined"
+                    onPress={() => {
+                      RNPermissions.request(value)
+                        .then(status => {
+                          showSnackbar(`request(${name})`, status);
+                        })
+                        .catch(error => {
+                          console.error(error);
+                        });
+                    }}>
+                    Request
+                  </Button>
+                </View>
+              </View>
+
+              <Divider />
+            </React.Fragment>
+          );
+        })}
+
+        <View style={{padding: 20, paddingBottom: 32}}>
+          <Text
+            numberOfLines={1}
+            style={{
+              color: 'rgba(0, 0, 0, 0.87)',
+              fontWeight: '400',
+              fontSize: 16,
+              textAlign: 'left',
+            }}>
+            NOTIFICATIONS
+          </Text>
+
+          <View style={{flexDirection: 'row', marginTop: 12}}>
+            <Button
+              compact={true}
+              icon="eye-outline"
+              mode="outlined"
+              onPress={() => {
+                RNPermissions.checkNotifications()
+                  .then(response => {
+                    showSnackbar('checkNotifications()', response);
+                  })
+                  .catch(error => {
+                    console.error(error);
+                  });
+              }}>
+              Check
+            </Button>
+
+            <View style={{width: 8}} />
+
+            <Button
+              compact={true}
+              icon="help-circle-outline"
+              mode="outlined"
+              onPress={() => {
+                const options: NotificationOption[] = [
+                  'alert',
+                  'badge',
+                  'sound',
+                ];
+
+                RNPermissions.requestNotifications(options)
+                  .then(response => {
+                    showSnackbar(
+                      `requestNotifications([${options
+                        .map(option => `"${option}"`)
+                        .join(', ')}])`,
+                      response,
+                    );
+                  })
+                  .catch(error => {
+                    console.error(error);
+                  });
+              }}>
+              Request
+            </Button>
+          </View>
+        </View>
+      </ScrollView>
+
+      <Snackbar
+        visible={snackbarContent != null}
+        duration={10000}
+        onDismiss={hideSnackbar}
+        action={{
+          color: '#607d8b',
+          label: 'Hide',
+          onPress: hideSnackbar,
+        }}>
+        {snackbarContent}
+      </Snackbar>
+    </View>
+  );
+};
--- FabricExample/index.js
+++ FabricExample/index.js
@@ -1,9 +1,14 @@
-/**
- * @format
- */
-
+import React from 'react';
 import {AppRegistry} from 'react-native';
-import App from './App';
+import {Provider as PaperProvider} from 'react-native-paper';
+import {App} from './App';
 import {name as appName} from './app.json';
+import theme from './theme';
 
-AppRegistry.registerComponent(appName, () => App);
+let Main = () => (
+  <PaperProvider theme={theme}>
+    <App />
+  </PaperProvider>
+);
+
+AppRegistry.registerComponent(appName, () => Main);
