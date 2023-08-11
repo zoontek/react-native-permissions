ead6e6ea8f77e97784c1b567f08294f63b11a635
Mathieu Acthernoene <zoontek@gmail.com>
2023-08-11 15:12:06 +0200

    prepare_react_native_permissions -> setup_permissions

--- scripts/permissions_setup.rb
+++ scripts/permissions_setup.rb
@@ -1,47 +1,15 @@
-require 'json'
 require 'fileutils'
-
-def pkg_dir(dir)
-  pkg_path = File.join(dir, 'package.json')
-
-  if File.exist?(pkg_path)
-    dir
-  else
-    parent_dir = File.expand_path('..', dir)
-
-    if parent_dir != dir
-      pkg_dir(parent_dir)
-    end
-  end
-end
 
 def log_warning(message)
   puts "[Permissions] #{message}"
 end
 
-def prepare_react_native_permissions!
-  config_key = 'reactNativePermissionsIOS'
+def setup_permissions(config)
+  if config.nil? || !config.is_a?(Array)
+    return log_warning("Invalid config argument")
+  end
 
   module_dir = File.expand_path('..', __dir__)
-  root_dir = pkg_dir(Dir.pwd) || Dir.pwd
-  pkg_path = File.join(root_dir, 'package.json')
-  json_path = File.join(root_dir, "#{config_key}.json")
-
-  config = JSON.parse(File.read(pkg_path))[config_key]
-
-  if !config && File.exist?(json_path)
-    text = File.read(json_path)
-    config = JSON.parse(text)
-  end
-
-  if !config
-    return log_warning("No #{config_key} config found")
-  end
-
-  unless config.is_a?(Array)
-    return log_warning("Invalid #{config_key} config")
-  end
-
   ios_dir = File.join(module_dir, 'ios')
   ios_dirents = Dir.entries(ios_dir).map { |entry| File.join(ios_dir, entry) }
 
