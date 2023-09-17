f6aa473f86e8b6147d91902f079e1416aec4a364
Mathieu Acthernoene <zoontek@gmail.com>
2023-09-17 13:00:18 +0200

    Simplify calendar / reminder permission handlers

--- ios/Calendars/RNPermissionHandlerCalendars.m
+++ ios/Calendars/RNPermissionHandlerCalendars.m
@@ -17,32 +17,16 @@
 
 - (void)checkWithResolver:(void (^ _Nonnull)(RNPermissionStatus))resolve
                  rejecter:(void (__unused ^ _Nonnull)(NSError * _Nonnull))reject {
-  EKAuthorizationStatus status = [EKEventStore authorizationStatusForEntityType:EKEntityTypeEvent];
-
-  if (@available(iOS 17.0, *)) {
-    switch (status) {
-      case EKAuthorizationStatusNotDetermined:
-        return resolve(RNPermissionStatusNotDetermined);
-      case EKAuthorizationStatusRestricted:
-        return resolve(RNPermissionStatusRestricted);
-      case EKAuthorizationStatusDenied:
-      case EKAuthorizationStatusWriteOnly:
-        return resolve(RNPermissionStatusDenied);
-      case EKAuthorizationStatusFullAccess:
-        return resolve(RNPermissionStatusAuthorized);
-    }
-  } else {
-    switch (status) {
-      case EKAuthorizationStatusNotDetermined:
-        return resolve(RNPermissionStatusNotDetermined);
-      case EKAuthorizationStatusRestricted:
-        return resolve(RNPermissionStatusRestricted);
-      case EKAuthorizationStatusDenied:
-      case EKAuthorizationStatusWriteOnly:
-        return resolve(RNPermissionStatusDenied);
-      case EKAuthorizationStatusAuthorized:
-        return resolve(RNPermissionStatusAuthorized);
-    }
+  switch ([EKEventStore authorizationStatusForEntityType:EKEntityTypeEvent]) {
+    case EKAuthorizationStatusNotDetermined:
+      return resolve(RNPermissionStatusNotDetermined);
+    case EKAuthorizationStatusRestricted:
+      return resolve(RNPermissionStatusRestricted);
+    case EKAuthorizationStatusDenied:
+    case EKAuthorizationStatusWriteOnly:
+      return resolve(RNPermissionStatusDenied);
+    case EKAuthorizationStatusFullAccess:
+      return resolve(RNPermissionStatusAuthorized);
   }
 }
 
@@ -50,23 +34,18 @@
                    rejecter:(void (^ _Nonnull)(NSError * _Nonnull))reject {
   EKEventStore *store = [EKEventStore new];
 
+  void (^completion)(BOOL, NSError * _Nullable) = ^(__unused BOOL granted, NSError * _Nullable error) {
+    if (error != nil) {
+      reject(error);
+    } else {
+      [self checkWithResolver:resolve rejecter:reject];
+    }
+  };
+
   if (@available(iOS 17.0, *)) {
-    [store requestFullAccessToEventsWithCompletion:^(__unused BOOL granted, NSError * _Nullable error) {
-      if (error != nil) {
-        reject(error);
-      } else {
-        [self checkWithResolver:resolve rejecter:reject];
-      }
-    }];
+    [store requestFullAccessToEventsWithCompletion:completion];
   } else {
-    [store requestAccessToEntityType:EKEntityTypeEvent
-                          completion:^(__unused BOOL granted, NSError * _Nullable error) {
-      if (error != nil) {
-        reject(error);
-      } else {
-        [self checkWithResolver:resolve rejecter:reject];
-      }
-    }];
+    [store requestAccessToEntityType:EKEntityTypeEvent completion:completion];
   }
 }
 
--- ios/Reminders/RNPermissionHandlerReminders.m
+++ ios/Reminders/RNPermissionHandlerReminders.m
@@ -17,32 +17,16 @@
 
 - (void)checkWithResolver:(void (^ _Nonnull)(RNPermissionStatus))resolve
                  rejecter:(void (__unused ^ _Nonnull)(NSError * _Nonnull))reject {
-  EKAuthorizationStatus status = [EKEventStore authorizationStatusForEntityType:EKEntityTypeReminder];
-
-  if (@available(iOS 17.0, *)) {
-    switch (status) {
-      case EKAuthorizationStatusNotDetermined:
-        return resolve(RNPermissionStatusNotDetermined);
-      case EKAuthorizationStatusRestricted:
-        return resolve(RNPermissionStatusRestricted);
-      case EKAuthorizationStatusDenied:
-      case EKAuthorizationStatusWriteOnly:
-        return resolve(RNPermissionStatusDenied);
-      case EKAuthorizationStatusFullAccess:
-        return resolve(RNPermissionStatusAuthorized);
-    }
-  } else {
-    switch (status) {
-      case EKAuthorizationStatusNotDetermined:
-        return resolve(RNPermissionStatusNotDetermined);
-      case EKAuthorizationStatusRestricted:
-        return resolve(RNPermissionStatusRestricted);
-      case EKAuthorizationStatusDenied:
-      case EKAuthorizationStatusWriteOnly:
-        return resolve(RNPermissionStatusDenied);
-      case EKAuthorizationStatusAuthorized:
-        return resolve(RNPermissionStatusAuthorized);
-    }
+  switch ([EKEventStore authorizationStatusForEntityType:EKEntityTypeReminder]) {
+    case EKAuthorizationStatusNotDetermined:
+      return resolve(RNPermissionStatusNotDetermined);
+    case EKAuthorizationStatusRestricted:
+      return resolve(RNPermissionStatusRestricted);
+    case EKAuthorizationStatusDenied:
+    case EKAuthorizationStatusWriteOnly:
+      return resolve(RNPermissionStatusDenied);
+    case EKAuthorizationStatusFullAccess:
+      return resolve(RNPermissionStatusAuthorized);
   }
 }
 
@@ -50,23 +34,18 @@
                    rejecter:(void (^ _Nonnull)(NSError * _Nonnull))reject {
   EKEventStore *store = [EKEventStore new];
 
+  void (^completion)(BOOL, NSError * _Nullable) = ^(__unused BOOL granted, NSError * _Nullable error) {
+    if (error != nil) {
+      reject(error);
+    } else {
+      [self checkWithResolver:resolve rejecter:reject];
+    }
+  };
+
   if (@available(iOS 17.0, *)) {
-    [store requestFullAccessToRemindersWithCompletion:^(__unused BOOL granted, NSError * _Nullable error) {
-      if (error != nil) {
-        reject(error);
-      } else {
-        [self checkWithResolver:resolve rejecter:reject];
-      }
-    }];
+    [store requestFullAccessToRemindersWithCompletion:completion];
   } else {
-    [store requestAccessToEntityType:EKEntityTypeReminder
-                          completion:^(__unused BOOL granted, NSError * _Nullable error) {
-      if (error != nil) {
-        reject(error);
-      } else {
-        [self checkWithResolver:resolve rejecter:reject];
-      }
-    }];
+    [store requestAccessToEntityType:EKEntityTypeReminder completion:completion];
   }
 }
 
