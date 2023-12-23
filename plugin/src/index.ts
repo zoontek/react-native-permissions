0c2b5c4a89d98098f25a3cece3a15c1f55f6d242
Jakov Glavina <jakov.glavina@icloud.com>
2023-12-23 17:44:18 +0100

    feat: setup expo config plugin for ios permissions setup

---
+++ plugin/src/index.ts
@@ -0,0 +1,52 @@
+import {mergeContents} from '@expo/config-plugins/build/utils/generateCode';
+import {ConfigPlugin, withDangerousMod} from 'expo/config-plugins';
+import {promises} from 'fs';
+import path from 'path';
+
+export function addPodfileNodeRequire(src: string): string {
+  return mergeContents({
+    tag: `node-require-function`,
+    src,
+    newSrc: `def node_require(script)\n\t# Resolve script with node to allow for hoisting\n\trequire Pod::Executable.execute_command('node', ['-p',\n\t\t"require.resolve(\n\t\t\t'#{script}',\n\t\t\t {paths: [process.argv[1]]},\n\t\t)", __dir__]).strip\nend\n\nnode_require('react-native-permissions/scripts/setup.rb')`,
+    anchor: /scripts\/react_native_pods/,
+    offset: 1,
+    comment: '#',
+  }).contents;
+}
+
+export function addPodfilePermissionsArray(src: string, permissions: string[]): string {
+  return mergeContents({
+    tag: `permissions-array`,
+    src,
+    newSrc: `setup_permissions([${permissions.map((p) => `'${p}'`).join(',')}])`,
+    anchor: /prepare_react_native_project!/,
+    offset: 1,
+    comment: '#',
+  }).contents;
+}
+
+export interface IOSPermissions {
+  /**
+   * Array of iOS permissions for which to setup permissions.
+   */
+  iosPermissions: string[];
+}
+
+const withIOSPodfilePermissions: ConfigPlugin<IOSPermissions> = (config, {iosPermissions}) => {
+  return withDangerousMod(config, [
+    'ios',
+    async (c) => {
+      const file = path.join(c.modRequest.platformProjectRoot, 'Podfile');
+
+      let contents = await promises.readFile(file, 'utf8');
+
+      contents = addPodfileNodeRequire(contents);
+      contents = addPodfilePermissionsArray(contents, iosPermissions);
+
+      await promises.writeFile(file, contents, 'utf-8');
+      return c;
+    },
+  ]);
+};
+
+export default withIOSPodfilePermissions;
