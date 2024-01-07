5b013ad2a0e1ea7caa73693b8191c100d977299d
Mathieu Acthernoene <zoontek@gmail.com>
2024-01-07 19:59:15 +0100

    Fix `AndroidManifest.xml` namespace deprecation warning

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,4 +1,25 @@
+const path = require('path');
+const pkg = require('../package.json');
+
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const escape = require('escape-string-regexp');
+const exclusionList = require('metro-config/src/defaults/exclusionList');
+
+const peerDependencies = Object.keys(pkg.peerDependencies);
+const root = path.resolve(__dirname, '..');
+const projectNodeModules = path.join(__dirname, 'node_modules');
+const rootNodeModules = path.join(root, 'node_modules');
+
+// We need to make sure that only one version is loaded for peerDependencies
+// So we block them at the root, and alias them to the versions in example's node_modules
+const blacklistRE = exclusionList(
+  peerDependencies.map((name) => new RegExp(`^${escape(path.join(rootNodeModules, name))}\\/.*$`)),
+);
+
+const extraNodeModules = peerDependencies.reduce((acc, name) => {
+  acc[name] = path.join(projectNodeModules, name);
+  return acc;
+}, {});
 
 /**
  * Metro configuration
@@ -6,6 +27,10 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  projectRoot: __dirname,
+  watchFolders: [root],
+  resolver: {blacklistRE, extraNodeModules},
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
