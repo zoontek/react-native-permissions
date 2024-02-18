abfe1f2caffeb2c0a9c4786cca99b7f410153600
Mathieu Acthernoene <zoontek@gmail.com>
2024-02-18 14:43:37 +0100

    Replace deprecated microphone API (#846)

--- ios/Microphone/RNPermissionHandlerMicrophone.m
+++ ios/Microphone/RNPermissionHandlerMicrophone.m
@@ -14,21 +14,38 @@
 
 - (void)checkWithResolver:(void (^ _Nonnull)(RNPermissionStatus))resolve
                  rejecter:(void (__unused ^ _Nonnull)(NSError * _Nonnull))reject {
-  switch ([[AVAudioSession sharedInstance] recordPermission]) {
-    case AVAudioSessionRecordPermissionUndetermined:
-      return resolve(RNPermissionStatusNotDetermined);
-    case AVAudioSessionRecordPermissionDenied:
-      return resolve(RNPermissionStatusDenied);
-    case AVAudioSessionRecordPermissionGranted:
-      return resolve(RNPermissionStatusAuthorized);
+  if (@available(iOS 17.0, *)) {
+    switch ([[AVAudioApplication sharedInstance] recordPermission]) {
+      case AVAudioApplicationRecordPermissionUndetermined:
+        return resolve(RNPermissionStatusNotDetermined);
+      case AVAudioApplicationRecordPermissionDenied:
+        return resolve(RNPermissionStatusDenied);
+      case AVAudioApplicationRecordPermissionGranted:
+        return resolve(RNPermissionStatusAuthorized);
+    }
+  } else {
+    switch ([[AVAudioSession sharedInstance] recordPermission]) {
+      case AVAudioSessionRecordPermissionUndetermined:
+        return resolve(RNPermissionStatusNotDetermined);
+      case AVAudioSessionRecordPermissionDenied:
+        return resolve(RNPermissionStatusDenied);
+      case AVAudioSessionRecordPermissionGranted:
+        return resolve(RNPermissionStatusAuthorized);
+    }
   }
 }
 
 - (void)requestWithResolver:(void (^ _Nonnull)(RNPermissionStatus))resolve
                    rejecter:(void (^ _Nonnull)(NSError * _Nonnull))reject {
-  [[AVAudioSession sharedInstance] requestRecordPermission:^(__unused BOOL granted) {
-    [self checkWithResolver:resolve rejecter:reject];
-  }];
+  if (@available(iOS 17.0, *)) {
+    [AVAudioApplication requestRecordPermissionWithCompletionHandler:^(__unused BOOL granted) {
+      [self checkWithResolver:resolve rejecter:reject];
+    }];
+  } else {
+    [[AVAudioSession sharedInstance] requestRecordPermission:^(__unused BOOL granted) {
+      [self checkWithResolver:resolve rejecter:reject];
+    }];
+  }
 }
 
 @end
